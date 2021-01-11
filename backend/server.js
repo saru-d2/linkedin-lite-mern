ad2d4e1e939af2beae3e42c7d650aa6a2b90290b
saravanan senthil <senthil_saravanan@rocketmail.com>
2021-01-11 23:25:19 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ MONGO_URL = 'mongodb://localhost:27017/'
 MONGO_DB = 'test'
 PORT = 5000
 
-mongoose.connect(MONGO_URL + MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true }).then( () => console.log('connected to mongoDB')).catch(err => console.log('err'));
+mongoose.connect(MONGO_URL + MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => console.log('connected to mongoDB')).catch(err => console.log('err'));
 mongoose.connection.once('open', () => console.log('connected'))
-.on('error', (error) => {
-    console.log("mongodb failed to connect :'(");
-    console.log(error);
-});
+    .on('error', (error) => {
+        console.log("mongodb failed to connect :'(");
+        console.log(error);
+    });
 console.log(`mongoose status:${mongoose.STATES[mongoose.connection.readyState]}`);
 const authRouter = require('./routes/authRouter')
 const app = express();
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //api routes
 app.use('/auth', authRouter);
-
-app.listen(PORT, function() {
+app.use(passport.initialize());
+app.use(passport.session());
+app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 });
